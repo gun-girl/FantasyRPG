aeb8166be657a2b8728392d28096b731e43e3dd5
Shadi <shadi.amini1996@gmail.com>
2024-10-09 14:19:17 +0300

    Added basic C# project

---
+++ FantasyRPG/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Print "Hello, World!" to the console
+        Console.WriteLine("Hello, World!");
+    }
+}
